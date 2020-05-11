835567b49c65398c889c72a9fa017b1be017b4c8
mrjlancaster <lancast001@gmail.com>
2020-05-10 23:23:23 -0500

    add operations

--- main.js
+++ main.js
@@ -16,19 +16,50 @@ class Calculator {
 	}
 
 	appendNumber(number) {
-		this.currentOperand = number
+		if (number === '.' && this.currentOperand.includes('.')) return
+		this.currentOperand = this.currentOperand.toString() + number.toString()
 	}
 
 	chooseOperation(operation) {
-
+		if (this.currentOperand === '') return
+		if (this.previousOperand !== '') {
+			this.compute()
+		}
+		this.operation = operation
+		this.previousOperand = this.currentOperand
+		this.currentOperand = ''
 	}
+	
 
 	compute() {
-
+		let computation
+		const prev = parseFloat(this.previousOperand)
+		const current = parseFloat(this.currentOperand)
+		if (isNaN(prev) || isNaN(current)) return
+		switch (this.operation) {
+			case '+':
+				computation = prev + current
+				break
+			case '-':
+				computation = prev - current
+				break
+			case '*':
+				computation = prev * current
+				break
+			case '/':
+				computation = prev / current
+				break
+				default:
+					return
+		}
+		this.currentOperand = computation
+		this.operation = undefined
+		this.previousOperand = ''
 	}
 
 	updateDisplay() {
 		this.currentOperandTextElement.innerText = this.currentOperand
+		this.previousOperandTextElement.innerText = this.previousOperand
 	}
  }
 
@@ -47,4 +78,16 @@ numberButtons.forEach(button => {
 		calculator.appendNumber(button.innerText)
 		calculator.updateDisplay()
 	})
+})
+
+operationButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText)
+		calculator.updateDisplay()
+	})
+})
+
+equalsButton.addEventListener('click', button => {
+	calculator.compute()
+	calculator.updateDisplay()
 })
