ea0a9894fa4524369fc2a08c82371859e5d90975
mrjlancaster <lancast001@gmail.com>
2020-05-11 17:58:56 -0500

    fixed equal button display result

--- main.js
+++ main.js
@@ -58,15 +58,32 @@ class Calculator {
 	}
 
 	getDisplayNumber(number) {
-		const floatNumber = parseFloat(number)
-		if (isNaN(floatNumber)) return ''
-		return floatNumber.toLocaleString('en')
+		const stringNumber = number.toString()
+		const integerDigits = parseFloat(stringNumber.split('.')[0])
+		const decimalDigits = stringNumber.split('.')[1]
+		let integerDisplay
+		if (isNaN(integerDigits)) {
+			integerDisplay = ''
+		} else {
+			integerDisplay = integerDigits.toLocaleString('en', {
+				maximumFractionDigits: 0})
+		}
+		if (decimalDigits != null) {
+			return `${integerDisplay}.${decimalDigits}`
+		} else {
+			return integerDisplay
+		}
 	} 
 
 	updateDisplay() {
 		this.currentOperandTextElement.innerText = 
 			this.getDisplayNumber(this.currentOperand)
-		this.previousOperandTextElement.innerText = this.previousOperand
+			if (this.operation != null) {
+				this.previousOperandTextElement.innerText = 
+					`${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+		} else {
+			this.previousOperandTextElement.innerText = ''
+		}
 	}
  }
 
@@ -97,7 +114,7 @@ operationButtons.forEach(button => {
 })
 
 equalsButton.addEventListener('click', button => {
-	calculator.clear()
+	calculator.compute()
 	calculator.updateDisplay()
 })
 
